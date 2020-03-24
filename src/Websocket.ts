bad63328c0a673ed9b1b466f48e0f1a99af62aa0
Philipp Mosch <p.mosch@babep.de>
2020-03-24 15:13:15 +0100

    erste Design-Studie

--- src/Websocket.ts
+++ src/Websocket.ts
@@ -4,7 +4,7 @@ export class NetManager{
 
     constructor(){
         this.socket = new WebSocket("ws://localhost:9000");
-
+        
         this.socket.addEventListener("open", (evt) => {
             this.socket.send(JSON.stringify({command: "player_register", payload: {name: "Herbert", passphrase: "1234"}}));
         });
